4be4d6a645fd7d704295c7bfb5d642cc3fb0ff74
levy <897895407@qq.com>
2018-07-25 18:09:53 +0800

    重命名common-data-table => basic

--- stories/index.js
+++ stories/index.js
@@ -1,22 +1,22 @@
 import {storiesOf} from '@storybook/vue'
 import 'element-ui/lib/theme-chalk/index.css'
-import CommonDataTable from './common-data-table.vue'
+import Basic from './basic.vue'
 import SearchDataTable from './search-data-table.vue'
 import TreeDataTable from './tree-data-table.vue'
 import CustomFormDataTable from './custom-form-data-table.vue'
 import CustomButtonDataTable from './custom-button-data-table.vue'
 
 storiesOf('ElDataTable', module)
-  .add('通用表格', commonTable)
+  .add('基本CRUD', basic)
   .add('查询表格', searchTable)
   .add('树形表格', treeTable)
   .add('自定义表单', customFormTable)
   .add('自定义操作按钮', customButtonTable)
 
-function commonTable() {
+function basic() {
   return {
-    components: {CommonDataTable},
-    template: `<common-data-table/>`
+    components: {Basic},
+    template: `<basic/>`
   }
 }
 function treeTable() {
