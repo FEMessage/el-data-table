41416c02044db0b967eba5f8abf9ddd0f283dd63
levy <897895407@qq.com>
2018-07-26 10:33:33 +0800

    调整story顺序

--- stories/index.js
+++ stories/index.js
@@ -3,15 +3,15 @@ import 'element-ui/lib/theme-chalk/index.css'
 import Basic from './basic.vue'
 import Search from './search.vue'
 import Tree from './tree.vue'
-import CustomRender from './custom-render.vue'
 import CustomButtons from './custom-buttons.vue'
+import CustomRender from './custom-render.vue'
 
 storiesOf('ElDataTable', module)
   .add('基本CRUD', basic)
   .add('基本查询', search)
   .add('树形结构', tree)
-  .add('自定义渲染', customRender)
   .add('自定义按钮', customButtons)
+  .add('自定义渲染', customRender)
 
 function basic() {
   return {
@@ -31,15 +31,15 @@ function search() {
     template: `<search></search>`
   }
 }
-function customRender() {
-  return {
-    components: {CustomRender},
-    template: `<custom-render/>`
-  }
-}
 function customButtons() {
   return {
     components: {CustomButtons},
     template: `<custom-buttons/>`
+  }
+}
+function customRender() {
+  return {
+    components: {CustomRender},
+    template: `<custom-render/>`
   }
 }
