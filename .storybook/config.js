5d3099c5832189521839d37d9e84ef50e6e09b53
levy <897895407@qq.com>
2018-07-26 10:32:39 +0800

    清晰代码意图

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import {configure, setAddon} from '@storybook/vue'
+import {configure} from '@storybook/vue'
 
 import Vue from 'vue'
 import axios from 'axios'
@@ -25,20 +25,24 @@ Vue.use(Button)
 Vue.use(Dialog)
 Vue.use(Form)
 Vue.use(FormItem)
-Vue.use(Input)
-Vue.use(Select)
-Vue.use(Option)
 Vue.use(Loading.directive)
 Vue.use(Pagination)
 Vue.use(Table)
 Vue.use(TableColumn)
-Vue.component('el-form-renderer', ElFormRenderer)
 
+// 注册表单渲染组件及表单组件
+// 如要使用其他表单组件, 如radio, 需要另外注册
+Vue.component('el-form-renderer', ElFormRenderer)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
+
+// 赋予table发送请求的能力
 Vue.prototype.$axios = axios
-Vue.prototype.$msgbox = MessageBox
-Vue.prototype.$alert = MessageBox.alert
+
+// 删除操作需要用到确认提示
 Vue.prototype.$confirm = MessageBox.confirm
-Vue.prototype.$prompt = MessageBox.prompt
+// 操作成功后需要用到消息提醒
 Vue.prototype.$message = Message
 
 function loadStories() {
