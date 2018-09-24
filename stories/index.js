f352608d471cda390cecf1a8a9e2f695273d6916
levy <897895407@qq.com>
2018-09-24 17:08:03 +0800

    重命名story

--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,7 @@ import Basic from './basic.vue'
 import Search from './search.vue'
 import Tree from './tree.vue'
 import CustomButtons from './custom-buttons.vue'
-import CustomRender from './custom-render.vue'
+import ExtraParams from './extra-params.vue'
 import CustomId from './custom-id.vue'
 
 storiesOf('ElDataTable', module)
@@ -12,8 +12,8 @@ storiesOf('ElDataTable', module)
   .add('基本查询', search)
   .add('树形结构', tree)
   .add('自定义按钮', customButtons)
-  .add('自定义渲染', customRender)
   .add('自定义主键', customId)
+  .add('slot=form及额外的post/put参数', extraParams)
 
 function basic() {
   return {
@@ -39,10 +39,10 @@ function customButtons() {
     template: `<custom-buttons/>`
   }
 }
-function customRender() {
+function extraParams() {
   return {
-    components: {CustomRender},
-    template: `<custom-render/>`
+    components: {ExtraParams},
+    template: `<extra-params/>`
   }
 }
 function customId() {
