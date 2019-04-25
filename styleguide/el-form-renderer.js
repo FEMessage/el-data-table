daf3421d1736031755183fefaa55e9df4474a152
evillt <ijoec123@gmail.com>
2019-04-25 15:44:20 +0800

    chore: update styleguide config

--- styleguide/el-form-renderer.js
+++ styleguide/el-form-renderer.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
-import FormRenderer from '@femessage/el-form-renderer/src'
+import FormRenderer from '@femessage/el-form-renderer'
 
 Vue.component('ElFormRenderer', FormRenderer)
