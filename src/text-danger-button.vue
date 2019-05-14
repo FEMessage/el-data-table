5499bc00231b564ca823899dc64ddf83bbb81a08
Donald Shen <825870831@qq.com>
2019-05-14 14:04:09 +0800

    feat: 删除按钮颜色使用class替代style

---
+++ src/text-danger-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="el-alert--error container">
+    <el-button v-bind="$attrs" v-on="$listeners" type="text" class="button">
+      <slot></slot>
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextDangerButton'
+}
+</script>
+
+<style lang="stylus" scoped>
+.container
+  display inline-block
+  background-color initial
+  .el-button + &
+    margin-left 10px
+
+.button
+  color inherit
+</style>
