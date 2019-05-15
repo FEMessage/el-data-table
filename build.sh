d817f06b10588576396cfcd7b62b6e9e4cb4473c
levy <897895407@qq.com>
2019-05-15 14:02:43 +0800

    optimize-release-process

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ yarn build
 git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/el-data-table.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
+
