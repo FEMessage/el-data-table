6b51bd04f425c5a919561154ebb30489e16840b9
levy <897895407@qq.com>
2019-05-15 14:23:11 +0800

    sleep 3s before notify

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ yarn build
 git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/el-data-table.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
-GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
-
