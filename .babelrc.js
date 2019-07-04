8216d8230936f4ae4a7a34583df17a275ae5cce5
EVILLT <ijoec123@gmail.com>
2019-07-04 17:00:03 +0800

    chore: up-to-date with cli (#160)

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,15 @@
-module.exports = api => ({
-  presets: [
-    ['@babel/preset-env', {modules: api.env('test') ? 'commonjs' : false}]
-  ],
-  plugins: [
-    [
-      '@babel/transform-runtime',
-      {
-        regenerator: true
-      }
+module.exports = api => {
+  return {
+    presets: [['@babel/env', {modules: api.env('test') ? 'commonjs' : false}]],
+    plugins: [
+      [
+        '@babel/transform-runtime',
+        {
+          regenerator: true,
+          // https://babeljs.io/docs/en/babel-plugin-transform-runtime#absoluteruntime
+          absoluteRuntime: true
+        }
+      ]
     ]
-  ]
-})
+  }
+}
