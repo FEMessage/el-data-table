16d5e16a0090396be9024a3038c42087489ddb77
colmugx <colmugx@qq.com>
2019-07-26 14:52:49 +0800

    chore

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,13 @@
 module.exports = api => {
   return {
     presets: [['@babel/env', {modules: api.env('test') ? 'commonjs' : false}]],
-    plugins: [['@babel/transform-runtime']]
+    plugins: [
+      [
+        '@babel/transform-runtime',
+        {
+          regenerator: true
+        }
+      ]
+    ]
   }
 }
