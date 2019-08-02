281f8fb5f6f62ef4ee14e0629745ab3ecda89c11
Han <xsytby1112@gmail.com>
2019-08-02 11:23:53 +0800

    docs: expose resetSearch as public method in api document

--- src/el-data-table.vue
+++ src/el-data-table.vue
@@ -21,14 +21,26 @@
         <slot name="search"></slot>
         <el-form-item>
           <!--https://github.com/ElemeFE/element/pull/5920-->
-          <el-button native-type="submit" type="primary" @click="search" size="small">查询</el-button>
+          <el-button
+            native-type="submit"
+            type="primary"
+            @click="search"
+            size="small"
+            >查询</el-button
+          >
           <el-button @click="resetSearch" size="small">重置</el-button>
         </el-form-item>
       </el-form-renderer>
 
       <el-form v-if="hasHeader">
         <el-form-item>
-          <el-button v-if="hasNew" type="primary" size="small" @click="onDefaultNew">{{ newText }}</el-button>
+          <el-button
+            v-if="hasNew"
+            type="primary"
+            size="small"
+            @click="onDefaultNew"
+            >{{ newText }}</el-button
+          >
           <self-loading-button
             v-for="(btn, i) in headerButtons"
             v-if="'show' in btn ? btn.show(selected) : true"
@@ -40,22 +52,28 @@
             :key="i"
             size="small"
           >
-            {{typeof btn.text === 'function' ? btn.text(selected) : btn.text}}
+            {{ typeof btn.text === 'function' ? btn.text(selected) : btn.text }}
           </self-loading-button>
           <el-button
             v-if="hasSelect && hasDelete"
             type="danger"
             size="small"
             @click="onDefaultDelete($event)"
-            :disabled="single ? (!selected.length || selected.length > 1) : !selected.length"
-          >删除</el-button>
+            :disabled="
+              single
+                ? !selected.length || selected.length > 1
+                : !selected.length
+            "
+            >删除</el-button
+          >
           <el-button
             v-if="canSearchCollapse"
             type="default"
             size="small"
             :icon="`el-icon-arrow-${isSearchCollapse ? 'down' : 'up'}`"
             @click="isSearchCollapse = !isSearchCollapse"
-          >{{ isSearchCollapse ? '展开' : '折叠' }}搜索</el-button>
+            >{{ isSearchCollapse ? '展开' : '折叠' }}搜索</el-button
+          >
           <!--@slot 额外的header内容, 当headerButtons不满足需求时可以使用，作用域传入selected -->
           <slot name="header" :selected="selected" />
         </el-form-item>
@@ -75,7 +93,10 @@
         <template v-if="isTree">
           <!--有多选-->
           <template v-if="hasSelect">
-            <el-table-column key="selection-key" v-bind="columns[0]"></el-table-column>
+            <el-table-column
+              key="selection-key"
+              v-bind="columns[0]"
+            ></el-table-column>
 
             <el-table-column key="tree-ctrl" v-bind="columns[1]">
               <template slot-scope="scope">
@@ -93,12 +114,12 @@
                   <i v-if="!scope.row._expanded" class="el-icon-plus"></i>
                   <i v-else class="el-icon-minus"></i>
                 </span>
-                {{scope.row[columns[1].prop]}}
+                {{ scope.row[columns[1].prop] }}
               </template>
             </el-table-column>
 
             <el-table-column
-              v-for="(col) in columns.filter((c, i) => i !== 0 && i !== 1)"
+              v-for="col in columns.filter((c, i) => i !== 0 && i !== 1)"
               :key="col.prop"
               v-bind="col"
             ></el-table-column>
@@ -123,12 +144,12 @@
                   <i v-if="!scope.row._expanded" class="el-icon-plus"></i>
                   <i v-else class="el-icon-minus"></i>
                 </span>
-                {{scope.row[columns[0].prop]}}
+                {{ scope.row[columns[0].prop] }}
               </template>
             </el-table-column>
 
             <el-table-column
-              v-for="(col) in columns.filter((c, i) => i !== 0)"
+              v-for="col in columns.filter((c, i) => i !== 0)"
               :key="col.prop"
               v-bind="col"
             ></el-table-column>
@@ -137,24 +158,31 @@
 
         <!--非树-->
         <template v-else>
-          <el-table-column v-for="(col) in columns" :key="col.prop" v-bind="col"></el-table-column>
+          <el-table-column
+            v-for="col in columns"
+            :key="col.prop"
+            v-bind="col"
+          ></el-table-column>
         </template>
 
         <!--默认操作列-->
-        <el-table-column label="操作" v-if="hasOperation" v-bind="operationAttrs">
+        <el-table-column
+          label="操作"
+          v-if="hasOperation"
+          v-bind="operationAttrs"
+        >
           <template slot-scope="scope">
             <text-button
               v-if="isTree && hasNew"
               @click="onDefaultNew(scope.row)"
-            >{{ newText }}</text-button>
-            <text-button
-              v-if="hasEdit"
-              @click="onDefaultEdit(scope.row)"
-            >{{ editText }}</text-button>
-            <text-button
-              v-if="hasView"
-              @click="onDefaultView(scope.row)"
-            >{{ viewText }}</text-button>
+              >{{ newText }}</text-button
+            >
+            <text-button v-if="hasEdit" @click="onDefaultEdit(scope.row)">{{
+              editText
+            }}</text-button>
+            <text-button v-if="hasView" @click="onDefaultView(scope.row)">{{
+              viewText
+            }}</text-button>
             <self-loading-button
               v-for="(btn, i) in extraButtons"
               v-if="'show' in btn ? btn.show(scope.row) : true"
@@ -165,13 +193,16 @@
               :key="i"
               is-text
             >
-              {{typeof btn.text === 'function' ? btn.text(scope.row) : btn.text}}
+              {{
+                typeof btn.text === 'function' ? btn.text(scope.row) : btn.text
+              }}
             </self-loading-button>
             <text-button
               v-if="!hasSelect && hasDelete && canDelete(scope.row)"
               type="danger"
               @click="onDefaultDelete(scope.row)"
-            >删除</text-button>
+              >删除</text-button
+            >
           </template>
         </el-table-column>
 
@@ -187,7 +218,7 @@
         :page-sizes="paginationSizes"
         :page-size="size"
         :total="total"
-        style="text-align: right; padding: 10px 0"
+        style="text-align: right; padding: 10px 0;"
         :layout="paginationLayout"
       ></el-pagination>
 
@@ -844,6 +875,11 @@ export default {
           })
       })
     },
+    /**
+     * 重置查询，相当于点击「重置」按钮
+     *
+     * @public
+     */
     resetSearch() {
       // reset后, form里的值会变成 undefined, 在下一次查询会赋值给query
       this.$refs.searchForm.resetFields()
@@ -1078,9 +1114,8 @@ export default {
 </script>
 <style lang="stylus">
 .el-data-table {
-  color-blue = #2196F3;
-  space-width = 18px;
-
+  color-blue = #2196F3
+  space-width = 18px
   .ms-tree-space {
     position: relative;
     top: 1px;
