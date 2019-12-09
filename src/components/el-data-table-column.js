1c289569b9ef42d9322851fa620fb4868b1a0347
gd4ark <gd.4ark@gmail.com>
2019-12-09 13:41:55 +0800

    feat: 支持多级表头

---
+++ src/components/el-data-table-column.js
@@ -0,0 +1,22 @@
+const defaltAttrs = {
+  align: 'center'
+}
+export default {
+  name: 'el-data-table-column',
+  functional: true,
+  render(h, {data, props}) {
+    let children = []
+    if (props.columns) {
+      children = props.columns.map(column =>
+        h('el-table-column', {
+          props: Object.assign({}, defaltAttrs, column)
+        })
+      )
+    }
+    data.props = {
+      ...defaltAttrs,
+      ...data.props
+    }
+    return h('el-table-column', data, children)
+  }
+}
