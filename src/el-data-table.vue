6f4d7731f8918589b3e506dc305e8031557d0437
Htongbing <36433396+Htongbing@users.noreply.github.com>
2019-12-24 16:25:38 +0800

    feat: 支持配置axios的config (#265)

--- src/el-data-table.vue
+++ src/el-data-table.vue
@@ -497,7 +497,7 @@ export default {
         const ids = Array.isArray(data)
           ? data.map(v => v[this.id]).join(',')
           : data[this.id]
-        return this.$axios.delete(this.url + '/' + ids)
+        return this.$axios.delete(this.url + '/' + ids, this.axiosConfig)
       }
     },
     /**
@@ -723,6 +723,15 @@ export default {
     buttonSize: {
       type: String,
       default: 'small'
+    },
+    /**
+     * 设置axios的config参数
+     */
+    axiosConfig: {
+      type: Object,
+      default() {
+        return {}
+      }
     }
   },
   data() {
@@ -865,7 +874,7 @@ export default {
       }
 
       this.$axios
-        .get(url + queryStr)
+        .get(url + queryStr, this.axiosConfig)
         .then(({data: resp}) => {
           let data = []
 
@@ -1028,7 +1037,7 @@ export default {
             ? ['post', this.url]
             : ['put', `${this.url}/${this.row[this.id]}`]
 
-          await this.$axios[method](url, data)
+          await this.$axios[method](url, data, this.axiosConfig)
         }
         this.getList()
         this.showMessage(true)
