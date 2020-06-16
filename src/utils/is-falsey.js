e8716f3d22e1a4ad923fb5da98e6efbfce37226f
snowlocked <wuzanh@gmail.com>
2020-06-16 16:32:25 +0800

    fix: searchform 为空数组时判断为空 no-data插槽存在时代理一下table方法，避免报错

--- src/utils/is-falsey.js
+++ src/utils/is-falsey.js
@@ -1,1 +1,3 @@
-export default value => ['', undefined, null].indexOf(value) > -1
+export default value =>
+  ['', undefined, null].indexOf(value) > -1 ||
+  (Array.isArray(value) && value.length === 0)
