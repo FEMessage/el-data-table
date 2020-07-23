cac6847f3788a8a10ff5334d6687d80267f1b0cd
levy <897895407@qq.com>
2020-07-23 13:36:00 +0800

    chore: add netlify.sh

--- notify.sh
+++ notify.sh
@@ -5,12 +5,16 @@ then
   exit 1
 fi
 
-git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/el-data-table.git > /dev/null 2>&1
+ORG_NAME=$(cut -d '/' -f 1 <<< "$TRAVIS_REPO_SLUG")
+
+REPO_NAME=$(cut -d '/' -f 2 <<< "$TRAVIS_REPO_SLUG")
+
+git remote add github https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
 GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
 
-url=https://api.github.com/repos/FEMessage/el-data-table/releases/latest
+url=https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/latest
 resp_tmp_file=resp.tmp
 
 curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
@@ -19,7 +23,7 @@ html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{pr
 body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
 version=$(echo $html_url | awk -F '/' '{print $NF}')
 
-msg='{"msgtype": "markdown", "markdown": {"title": "el-data-table更新", "text": "@所有人\n# [el-data-table('$version')]('$html_url')\n'$body'"}}'
+msg='{"msgtype": "markdown", "markdown": {"title": "$REPO_NAME更新", "text": "@所有人\n# [$REPO_NAME('$version')]('$html_url')\n'$body'"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
